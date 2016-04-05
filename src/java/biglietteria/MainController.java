5b5455801a37a85b08ee0d5a53160d242c135fa0
FSEVERI\scagnellato3082 <FSEVERI\scagnellato3082@lmatbi-26.fseveri.it>
2016-04-05 08:33:39 +0200

    inizio

---
+++ src/java/biglietteria/MainController.java
@@ -0,0 +1,29 @@
+package biglietteria;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author FSEVERI\scagnellato3082
+ */
+@Controller
+public class MainController {
+    
+
+    public MainController() {
+    }    
+
+    @RequestMapping(value="/")
+    public String index(ModelMap map){
+        return "index";
+    }
+    
+}
